7446361d2a5269c13ae7c570e908799ef5c4dd9b
Nagi <demon0315@naver.com>
2021-03-21 15:27:37 +0900

    django + react test DONE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    posts: [],
+  };
+
+  async getPosts() {
+    axios
+      .get("http://127.0.0.1:8000/api/")
+      .then(({ data }) => {
+        console.log(data);
+        this.setState({ posts: data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.posts.map((item) => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <span>{item.content}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
